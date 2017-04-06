2180bcf393d162eff371010ebac6863d1cbdd3d1
Aurélien Klein <aurelien.klein@viacesi.fr>
2017-04-06 11:55:11 +0200

    Add files via upload

--- requetes_sql/temperature.sql
+++ requetes_sql/temperature.sql
@@ -1,4 +1,12 @@
-SELECT Preparation.Temperature, Produit.NomProduit
-FROM (Preparation INNER JOIN Produit ON Preparation.IDproduit = Produit.IDproduit)
-WHERE Preparation.Temperature > 0
-ORDER BY Temperature ASC;
+DELIMITER |
+CREATE PROCEDURE temperature()
+
+BEGIN 
+
+	SELECT Preparation.Temperature, Produit.NomProduit
+	FROM (Preparation INNER JOIN Produit ON Preparation.IDproduit = Produit.IDproduit)
+	WHERE Preparation.Temperature > 0
+	ORDER BY Temperature ASC;
+    
+END|
+DELIMITER ;
